86aed2bbc21749d410c1494cf1ce398804c0837d
ameliablair <ameliablair@gmail.com>
2014-03-12 18:56:01 -0400

    starting the ratr app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ratr::Application.config.secret_key_base = '652d1fa5cd7205cb7fe5583982af294bc7f7013a85d7c9e5bdad85644c7a896585ffc9d7f6089091c1bdec95261053107f0e10e4a9777365a687850fc191a316'
